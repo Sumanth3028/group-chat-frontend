f4c17e34c4709a78567f1e61d1c59af1ed515d49
Sumanth3028 <sumanthakula05@gmail.com>
2023-09-22 12:46:09 +0530

    chat app dummy ui

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from "react";
 import SignUp from "./components/Auth/Sign-up";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
-
+import ChatUi from "./components/Chat-box/ChatUi";
 
 function App() {
   return (
-    <div className=" flex justify-center items-center bg-gradient-to-r from-blue-500  to-green-400 h-screen" >
+    <div className=" flex justify-center items-center bg-gradient-to-r from-blue-500  to-green-400 h-full">
       <Routes>
-        <Route path="/" element={<SignUp/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/chatapp" element={<ChatUi />}></Route>
       </Routes>
-    
     </div>
   );
 }
